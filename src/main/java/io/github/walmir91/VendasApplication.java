26046a19c2c41fa96ae66f9b3aa89f7c376d8e9e
walmir91 <walmirlmjr@gmail.com>
2021-12-04 12:41:36 -0300

    Adicionadas as dependencias WEB, JPA, SECURITY e TEST

--- src/main/java/io/github/walmir91/VendasApplication.java
+++ src/main/java/io/github/walmir91/VendasApplication.java
@@ -3,8 +3,10 @@ package io.github.walmir91;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class VendasApplication {
+
 
     public static void main(String[] args) {
 
